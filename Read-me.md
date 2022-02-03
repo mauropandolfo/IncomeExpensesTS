fe0c6096ee7f317271895834bff4e38e8a3ed8a1
Mauro Pandolfo <pandolfomauro@gmail.com>
2022-02-03 20:39:32 -0300

    Functional Frontend logic with TS.

---
+++ Read-me.md
@@ -0,0 +1,4 @@
+## Typescript App.
+Simple app to manage income and expenses of money built using TypeScript.
+
+![img](./ts.git)
---
+++ public/script/main.js
@@ -0,0 +1,113 @@
+import { Account, CategoryEnum, Entry } from "./Account.js";
+var account;
+var initialAcount = getAccountFromStorage();
+if (initialAcount) {
+    account = new Account(initialAcount);
+    updateBlanaceAmount(account);
+}
+else {
+    account = createInitialAccount();
+    updateBlanaceAmount(account);
+}
+function createInitialAccount() {
+    var setupAccount = new Account();
+    var expenseEjemplo = new Entry('Ejemplo gasto', 100, CategoryEnum.expense);
+    var incomeEjemplo = new Entry('Ejemplo ingreso', 150, CategoryEnum.income);
+    setupAccount.addEntry(expenseEjemplo);
+    setupAccount.addEntry(incomeEjemplo);
+    return setupAccount;
+}
+function getAccountFromStorage() {
+    var accountFromStorage = localStorage.getItem('account');
+    return accountFromStorage ? JSON.parse(accountFromStorage) : false;
+}
+function setAccountToStorage(account) {
+    localStorage.setItem('account', JSON.stringify(account));
+}
+function updateBlanaceAmount(account) {
+    var balanceAmountHtmlElement = document.querySelector('#balanceAmount');
+    var balanceAccount = account.getBalance();
+    balanceAmountHtmlElement.textContent = "".concat(balanceAccount, " $");
+}
+var entryTemplate = document.querySelector('#entryTemplate');
+var fragment = document.createDocumentFragment();
+var recordsContainer = document.querySelector('#recordsContainer');
+recordsContainer.onclick = function (event) {
+    if (event.target instanceof SVGElement && event.target.dataset.id) {
+        var elementId = event.target.dataset.id;
+        var entryElement = document.querySelector("[data-id=\"".concat(elementId, "\"]"));
+        deleteElement(elementId, entryElement);
+    }
+};
+var entries = account.getEntries();
+entries.forEach(function (entry) {
+    printEntry(entry);
+});
+function printEntry(entry) {
+    var concept = entry.concept, amount = entry.amount, category = entry.category, id = entry.id;
+    var entryConceptTemplate = entryTemplate.content.querySelector('.entryConcept');
+    var entryAmountTemplate = entryTemplate.content.querySelector('.entryAmount');
+    var entryContainerTemplate = entryTemplate.content.querySelector('div');
+    var iconSvg = entryTemplate.content.querySelector('svg');
+    var svgPath = entryTemplate.content.querySelector('path');
+    if (!entryConceptTemplate || !entryAmountTemplate || !entryContainerTemplate || !iconSvg || !svgPath) {
+        return;
+    }
+    entryConceptTemplate.textContent = concept;
+    entryContainerTemplate.setAttribute('data-id', String(id));
+    iconSvg.setAttribute('data-id', String(id));
+    svgPath.setAttribute('data-id', String(id));
+    if (category === CategoryEnum.expense) {
+        entryAmountTemplate.classList.add('text-indigo-500');
+        entryAmountTemplate.classList.remove('text-blue-500');
+        entryAmountTemplate.textContent = "-".concat(amount, " $");
+    }
+    else {
+        entryAmountTemplate.classList.add('text-blue-500');
+        entryAmountTemplate.classList.remove('text-indigo-500');
+        entryAmountTemplate.textContent = "".concat(amount, " $");
+    }
+    var clone = entryTemplate.content.cloneNode(true);
+    fragment.appendChild(clone);
+    recordsContainer.appendChild(fragment);
+}
+function deleteElement(id, entryElement) {
+    var _a, _b;
+    var entryConcept = (_a = entryElement.querySelector('.entryConcept')) === null || _a === void 0 ? void 0 : _a.textContent;
+    var entryAmount = (_b = entryElement.querySelector('.entryAmount')) === null || _b === void 0 ? void 0 : _b.textContent;
+    Swal.fire({
+        title: 'Estas seguro que deseas borrar esta entrada?',
+        text: " Concepto ".concat(entryConcept, "\n                Cantidad: ").concat(entryAmount, "\n                "),
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#63b5f7',
+        cancelButtonColor: '#7240ff',
+        confirmButtonText: 'Si, borrar entrada.',
+        cancelButtonText: 'No, no quiero borrar.'
+    }).then(function (result) {
+        if (result.isConfirmed) {
+            account.deleteEntryById(Number(id));
+            setAccountToStorage(account);
+            entryElement.remove();
+            updateBlanaceAmount(account);
+            Swal.fire('Borrado', 'La entrada ha sido borrada', 'success');
+        }
+    });
+}
+var entryConceptInput = document.querySelector('#entryName');
+var entryAmountInput = document.querySelector('#entryAmount');
+var addIncomeButton = document.querySelector('#addIncomeButton');
+var addExpenseButton = document.querySelector('#addExpenseButton');
+addIncomeButton.addEventListener('click', addEntryFromTemplate.bind(this, CategoryEnum.income));
+addExpenseButton.addEventListener('click', addEntryFromTemplate.bind(this, CategoryEnum.expense));
+function addEntryFromTemplate(category) {
+    var conceptValue = entryConceptInput.value;
+    var amountValue = entryAmountInput.value;
+    if (conceptValue && amountValue) {
+        var entryFromValues = new Entry(conceptValue, Number(amountValue), category);
+        account.addEntry(entryFromValues);
+        setAccountToStorage(account);
+        printEntry(entryFromValues);
+        updateBlanaceAmount(account);
+    }
+}
